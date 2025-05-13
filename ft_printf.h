593ddb29c63d887f40ebdc613223689407474532
Miguel Santos De Matos Ramirez Fernandes <migusant@c2r3s2.42lisboa.com>
2025-05-13 13:36:24 +0100

    Add printf header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,23 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 12:14:44 by migusant          #+#    #+#             */
-/*   Updated: 2025/05/09 12:14:45 by migusant         ###   ########.fr       */
+/*   Updated: 2025/05/13 13:16:23 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putnbr(int n);
+int		ft_putunbr(unsigned int n);
+int		ft_puthex(unsigned long n, char format);
+int		ft_putptr(void *ptr);
+
+#endif
