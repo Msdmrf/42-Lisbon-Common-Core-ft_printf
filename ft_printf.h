7417072eeae63cfd7a10513c3309f2da7a93ed5f
Miguel Santos De Matos Ramirez Fernandes <migusant@c2r3s2.42lisboa.com>
2025-05-13 16:24:52 +0100

    Change ft_printf.h 2

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 12:14:44 by migusant          #+#    #+#             */
-/*   Updated: 2025/05/13 16:21:24 by migusant         ###   ########.fr       */
+/*   Updated: 2025/05/13 16:24:28 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		ft_printf(const char *format, ...);
-int		ft_putchar(char c);
-int		ft_putstr(char *str);
-int   ft_putnbr(unsigned int n, int is_signed);
-int		ft_puthex(unsigned long n, char format);
-int		ft_putptr(void *ptr);
+int   ft_printf(const char *format, ...);
+int   ft_putchar(char c);
+int   ft_putstr(char *str);
+int   ft_putnb(unsigned int n, int is_signed);
+int   ft_puthex(unsigned long n, char format);
+int   ft_putptr(void *ptr);
 
 #endif
